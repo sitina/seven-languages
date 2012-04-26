19ff10346c752ec7fb1f9256bca6dde050d5dc2f
Jiri Sitina <jirka.sitina@gmail.com>
2012-04-26 22:21:41 +0200

    Huh, there were no comments in the example before - added

--- ruby/test1.rb
+++ ruby/test1.rb
@@ -1,23 +1,36 @@
+# just print test
 puts 'Test'
+
+# set variable name
 name = 'My name'
+
+# use variable in text
 puts "name is #{name}"
 
+# find index of Ruby in String
 a = "hello Ruby"
 b = a.index("Ruby")
 puts b
 
+# while loop 10 times
 x = 0
 while (x < 10)
 	puts a
 	x = x + 1
 end
 
+# for loop 10 times 
+for i in 1..10 do
+	puts "line #{i}"
+end
+
+# read from console
 puts "Enter your name: "
 b = gets 
 puts "your name is #{b}"
 
 
-
+# make random integer and then guess it
 a = rand(10)
 x = -1
 
