133105f408420928c9cd6b0438ad1cc01fe12746
Jiri Sitina <jiri.sitina@gooddata.com>
2012-06-13 13:40:08 +0200

    added some ruby test classes

--- ruby/Tree2.rb
+++ ruby/Tree2.rb
@@ -7,15 +7,19 @@ class Tree2
 	end
 
 	def visit_all(&block)
-		visit &block
-		if (nodes.class.method_defined? :each)
-			nodes.each { |k, v| Tree2.new(v).visit_all &block }
+		# visit &block
+		if (block.class.method_defined? :each)
+			block.each { |k, v| Tree2.new(v).visit_all(&block) }
+		else
+			puts "aaa" #&block
 		end
 	end
 
 	def visit(&block)
 		if (nodes.class.method_defined? :each)
-			nodes.each { |k, v| puts k }
+			nodes.each { |k, v| puts k visit_all( v) }
+		else
+			puts "tree2Node #{nodes}"
 		end
 		# block.call self
 	end
--- ruby/test2-tasks.rb
+++ ruby/test2-tasks.rb
@@ -7,3 +7,26 @@ end
 puts a
 
 a.each { |v| puts v } 
+
+
+
+
+
+puts('regexp test')
+# ruby regexp test
+
+fileName = 'test2-tasks.rb'
+
+def fileReadTest(fileName, pattern)
+	counter = 1
+	file = File.new(fileName, "r")
+	while (line = file.gets)
+		if (/#{pattern}/.match(line) != nil) 
+			puts "#{counter}: #{line}"
+			counter = counter + 1
+		end
+	end
+	file.close
+end
+
+fileReadTest(fileName, 'counter')
